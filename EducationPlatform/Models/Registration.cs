56b65db789b4faa4d0ba1d557e1e5ac23c05fe8f
Dizare <Dizare@DESKTOP-87KNLMM>
2024-05-15 08:06:45 +0500

    Добавьте файлы проекта.

---
+++ EducationPlatform/Models/Registration.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EducationPlatform.Models
+{
+    public class Registration
+    {
+        [Required(ErrorMessage = "Имя не введено")]
+        public string Name { get; set; }
+        [Required(ErrorMessage = "Фамилия не введена")]
+        public string SecondName { get; set; }
+        [Required(ErrorMessage = "Неправильная почта")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Неправильный пароль")]
+        public string Password { get; set; }
+    }
+}
